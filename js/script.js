86fd09e7d6a73fa261a4af7ad19edde9e83ec3e9
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-24 16:29:25 +0200

    sanck-2-set

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/*
 const vipList =
 [
     'Brad Pitt', 
@@ -33,3 +33,46 @@ vipList.forEach((vip, index) =>
 })
 
 console.log(tableVip);
+*/
+
+
+// snack 2
+
+const student = 
+[
+    {
+        id: 213,
+        name: "Marco della Rovere",
+        grades: 78
+    },
+    {
+        id: 110,
+        name: "Paola Cortellessa",
+        grades: 96
+    },
+    {
+        id: 250,
+        name: "Andrea Mantegna",
+        grades: 48
+    },
+    {
+        id: 145,
+        name: "Gaia Borromini",
+        grades: 74
+    },
+    {
+        id: 196,
+        name: "Luigi Grimaldello",
+        grades: 68
+    },
+    {
+        id: 102,
+        name: "Piero della Francesca",
+        grades: 50
+    },
+    {
+        id: 120,
+        name: "Francesca da Polenta",
+        grades: 84
+    }
+];
